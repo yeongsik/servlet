b51ddb9b9c88ae7f07653e559ed3bf107636cc24
LAPTOP-N91UPOS7\dudtl <youngsik0266@gmail.com>
2021-07-30 20:07:24 +0900

    2021/07/30

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,50 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+
+/*
+* 1. 파라미터 전송 기능
+* http://localhost:8080/request-param?username=hello&age=20
+* */
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("[전체 파라미터 조회] - start");
+//        Enumeration<String> parameterNames = request.getParameterNames();
+
+        // 요즘 방식 전체 파라미터 조회
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        // 단일 파라미터 조회
+        System.out.println("[단일 파라미터 조회] - start");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println("[단일 파라미터 조회] - end ");
+
+//      하나의 파라미터 이름이 여러개 값이 들어올때 -> 내부 우선순위에서 먼저 잡히는 애가 나오게 된다.
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames = request.getParameterValues("username");
+        for (String s : usernames) {
+            System.out.println("username = " + s);
+        }
+        // 중복으로 보내는 경우 거의 없다.
+
+    }
+}
---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -0,0 +1,10 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+
+import java.util.Map;
+
+public interface ControllerV3 {
+    // 서블릿 종속성 탈피
+    ModelView process(Map<String, String> paramMap);
+}
